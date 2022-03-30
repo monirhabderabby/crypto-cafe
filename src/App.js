b9a5041f008e59ec3858941e1c5866fa683520a8
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-03-30 22:24:50 +0600

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Routes, Route} from "react-router-dom";
 import Homepage from './components/Homepage/Homepage';
+import Header from './components/Homepage/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Homepage></Homepage>}></Route>
       </Routes>
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -4,7 +4,9 @@ const Homepage = () => {
     return (
         <div className='md:px-16 py-8'>
             <div className='flex flex-col justify-center h-[80vh] items-center'>
-                <h1 className='font-mono text-xl md:text-5xl'>Welcome to CRYPTO CAFE</h1>
+                <h1 className='font-mono text-xl md:text-5xl'>Welcome to CRYPTO CAFE <span className='text-6xl'>☕</span>
+                
+                </h1>
                 <button className='py-2 px-3 text-white  mt-3 rounded-full bg-cyan-600'>
                     Explore Coins
                 </button>
