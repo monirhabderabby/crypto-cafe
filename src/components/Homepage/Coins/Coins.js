10639d41cd05dd671e477ba7aabb0b9fbe7107b3
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-03-31 07:35:49 +0600

    update react 18

--- src/components/Homepage/Coins/Coins.js
+++ src/components/Homepage/Coins/Coins.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Coins = () => {
+    const [coins, setCoins] = useState([])
+    useEffect( () => {
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
+        .then(res=> res.json())
+        .then(data=> setCoins(data))
+    }, [])
     return (
         <div>
-            This is coin page
+            {
+                console.log(coins)
+            }
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
