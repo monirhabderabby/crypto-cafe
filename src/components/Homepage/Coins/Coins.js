21c0103d0f868dc2a164ea6c6776a2bf97b68588
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-03-31 10:02:41 +0600

    site completed

--- src/components/Homepage/Coins/Coins.js
+++ src/components/Homepage/Coins/Coins.js
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import CoinCard from '../../CoinCard/CoinCard';
+import Spinner from '../../Spinner/Spinner';
+
 
 const Coins = () => {
     const [coins, setCoins] = useState([])
+    const [loading, setLoading] = useState(false)
     useEffect( () => {
+        setLoading(true)
         fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
         .then(res=> res.json())
         .then(data=> {
             console.log(data);
             setCoins(data)
         })
+        setLoading(false)
     }, [])
     return (
-        <div className='px-4 pt-20 pb-24 mx-auto max-w-7xl ms:px-2'>
+        <div>
+        {loading ? <Spinner /> : <div className='px-4 pt-20 pb-24 mx-auto max-w-7xl ms:px-2'>
             <p className='text-center text-3xl font-bold text-gray-500'>
                 Available Crypto Currencies
             </p>
@@ -25,6 +31,8 @@ const Coins = () => {
                 coins.map(coin => <CoinCard coin={coin} key={coin.id}></CoinCard>) 
             }
             </div>
+        </div>}
+
         </div>
     );
 };
